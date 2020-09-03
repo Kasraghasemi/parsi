787022790dd4c719afb6a6913a8af918931e2623
Kasra Ghasemi <kasra0gh@bu.edu>
2020-09-03 11:34:40 -0400

    changing test for multi systems

--- test/test_multi_systems.py
+++ test/test_multi_systems.py
@@ -14,27 +14,22 @@ except:
     raise ModuleNotFoundError("parsi package is not installed properly")
 landa=0.001
 delta=0.1
-number_of_subsystems=5
+number_of_subsystems=2
 n=2*number_of_subsystems
 m=1*number_of_subsystems
 
-#A=np.random.rand(n,n)* landa
+A=np.random.rand(n,n)* landa
 #B=np.random.rand(n,m)* landa
-A=np.zeros((n,n))
+#A=np.zeros((n,n))
 B=np.zeros((n,m))
 
 for i in range(number_of_subsystems): 
     A[2*i:2*(i+1),2*i:2*(i+1)]= np.array([[1,1],[0,1]]) * delta
     B[2*i:2*(i+1),i]= np.array([0,1]) * delta
 
-A[1,2]=0.01
-A[2,1]=0.01
-A[3,4]=0.01
-A[4,3]=0.01
-
-W=pp.zonotope(G=np.eye(n)*0.01,x=np.zeros(n))
+W=pp.zonotope(G=np.eye(n)*0.1,x=np.zeros(n))
 X=pp.zonotope(G=np.eye(n),x=np.zeros(n),color='red')
-U=pp.zonotope(G=np.eye(m)*10,x=np.zeros(m))
+U=pp.zonotope(G=np.eye(m),x=np.zeros(m))
 
 system=parsi.Linear_system(A,B,W=W,X=X,U=U)
 sub_sys=parsi.sub_systems(system,partition_A=[2]*number_of_subsystems,partition_B=[1]*number_of_subsystems)
@@ -54,7 +49,7 @@ system.state= np.array( [sub_sys[i].state for i in range(number_of_subsystems)] 
 path=system.state.reshape(-1,1)
 
 # fig, axs = plt.subplots(ceil((number_of_subsystems)**0.5),ceil((number_of_subsystems)**0.5))
-fig, axs = plt.subplots(1,5)
+fig, axs = plt.subplots(1,number_of_subsystems)
 for i in range(number_of_subsystems):
     sub_sys[i].X.color='red'
     pp.visualize([sub_sys[i].X,omega[i]], ax = axs[i],fig=fig, title='')
